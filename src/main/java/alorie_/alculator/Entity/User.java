65e0e26c7db5fe1b88f7e534491e187d8c1fbe7f
Eduard <demanin@bk.ru>
2025-03-20 13:36:29 +0300

    Добавил сущности и докер

--- src/main/java/alorie_/alculator/Entity/User.java
+++ src/main/java/alorie_/alculator/Entity/User.java
@@ -12,20 +12,27 @@ import javax.validation.constraints.*;
 @Setter
 @AllArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @NotEmpty(message = "Имя не должно быть пустым")
     private String name;
+
     @Email
     @NotEmpty(message = "Email не должен быть пустым")
     private String email;
+
     @NotEmpty(message = "Возраст не должен быть пустым")
     private int age;
+
     @NotEmpty(message = "Поле не должно быть пустым")
     private int weight;
+
     @NotEmpty(message = "Поле не должно быть пустым")
     private int height;
+
     @NotEmpty(message = "Укажите пожалуйста цель")
     @Enumerated(value = EnumType.STRING)
     private Goal goal;
